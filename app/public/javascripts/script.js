00435fbe0b58fb5616c0869dc8492fbca5444d50
HankaKourimska <hanka.kourimska@yahoo.co.uk>
2018-07-29 15:21:08 +0200

    first boxes

--- app/public/javascripts/script.js
+++ app/public/javascripts/script.js
@@ -1,12 +1,22 @@
-// Create a "close" button and append it to each list item
+// Create the "close, paid, member" buttons and append them to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
+  var paid = document.createElement("PAID");
+  var txt2 = document.createTextNode("\u00D8");
+  var member = document.createElement("MEMBER");
+  var txt3 = document.createTextNode("M");
   span.className = "close";
+  paid.className = "paid";
+  member.className = "member";
   span.appendChild(txt);
+  paid.appendChild(txt2);
+  member.appendChild(txt3);
   myNodelist[i].appendChild(span);
+  myNodelist[i].appendChild(paid);
+  myNodelist[i].appendChild(member);
 }
 
 // Click on a close button to hide the current list item
@@ -42,8 +52,20 @@ function newElement() {
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
+  var paid = document.createElement("PAID");
+  var txt2 = document.createTextNode("\u00D8");
+  var member = document.createElement("MEMBER");
+  var txt3 = document.createTextNode("nM");
+  if(document.getElementById("memberBox").checked)txt3 = document.createTextNode("M");
   span.className = "close";
+  paid.className = "paid";
+  member.className = "member";
   span.appendChild(txt);
+  paid.appendChild(txt2);
+  member.appendChild(txt3);
+  myNodelist[i].appendChild(span);
+  myNodelist[i].appendChild(paid);
+  myNodelist[i].appendChild(member);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
